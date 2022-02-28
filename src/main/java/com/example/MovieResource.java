3a5b335b7328c9aa51c34e424bf8c4401d6b6e30
Pietro <pietro.locatelli00@gmail.com>
2022-02-28 12:13:10 +0100

    first crud test

--- src/main/java/com/example/MovieResource.java
+++ src/main/java/com/example/MovieResource.java
@@ -1,12 +1,12 @@
 package com.example;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/movies")
 public class MovieResource {
@@ -26,5 +26,39 @@ public class MovieResource {
         return movies.size();
     }
 
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response createMovie(String newMovie){
+        movies.add(newMovie);
+        return Response.ok(movies).build();
+    }
+
+    @PUT
+    @Path("{movieToUpdate}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response updateMovie(
+        @PathParam("movieToUpdate") String movieToUpdate,
+        @QueryParam("movie") String updateMovie
+        ){
+
+       movies = movies.stream().map(movie->{
+            if(movie.equals(movieToUpdate)){
+                return updateMovie;
+            }
+            return movie;
+        }).collect(Collectors.toList());
+        return Response.ok(movies).build();
+    }
+
+    @DELETE
+    @Path("{movieToDelete}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response deleteMovie(
+        @PathParam("movieToDelete") String movieToDelete){
+        boolean removed = movies.remove(movieToDelete);
+        return removed ? Response.noContent().build() : Response.status(Response.Status.BAD_REQUEST).build();
+    }
 
 }
